b25fe3c81a4725a148da3fafbc308f5539bf10c8
Islom_dev <inogomovislom@gmail.com>
2024-01-24 16:54:20 +0500

    fayllar qo'shildi

---
+++ index.ts
@@ -0,0 +1,62 @@
+import express from 'express';
+import "dotenv/config";
+import http from 'http';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import userRouter from './userRoutes';
+import { Server } from 'socket.io'
+import { User } from './userModels';
+import jwt from 'jsonwebtoken'
+const app = express()
+const server = http.createServer(app)
+const { PORT } = process.env || 4000
+
+app.use(cors())
+app.use(express.json())
+
+
+
+mongoose.connect(process.env.MONGO_URL!)
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', () => {
+    console.log('Connected to Mongo');  
+    server.listen(PORT, () => {
+     console.log('listening on port');        
+    }) 
+})
+
+const io = new Server(server, {
+    cors: {
+        origin:"https://master--tourmaline-beijinho-d5f39a.netlify.app/"
+    }
+})
+
+io.on("connection", (socket) => {
+    socket.on("private message", async(to, message, myself) => {
+        const user = await User.find({email: to})
+        const decoded = jwt.verify(myself, process.env.ACCESS_TOKEN_SECRET!)
+        const sender = await User.findById(decoded)
+        io.sockets.emit("refresh", "new Message");
+
+        if (user) {
+            user[0].messages.push({
+                reciver: user[0].email,
+                message,
+                sender: sender?.email,
+                time: new Date()
+            })
+            sender?.messages.push({
+                reciver: user[0].email,
+                message,
+                sender: sender?.email,
+                time: new Date()
+            })
+            await user[0].save();
+            await sender?.save();
+        }
+    })
+})
+
+app.use('/', userRouter)
---
+++ userModels.ts
@@ -0,0 +1,21 @@
+import mongoose, { mongo } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name:String,
+    imageId:String,
+    email:{
+        type:String,
+        unique:true
+    },
+    messages:[
+        {
+            message:String,
+            sender:String,
+            reciver:String,
+            time:Date
+        }
+    ]
+})
+
+
+export const User = mongoose.model("User", userSchema, "chatgram_users")
